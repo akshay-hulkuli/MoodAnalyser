95a634aac6f974283ddcfd4560336a66ef6b3a37
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-10 18:48:28 +0530

    ADDED: junit test methods to test null and empty string cases

--- lib/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
+++ lib/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
@@ -2,27 +2,55 @@ package com.bridgelabz.moodanalyzer;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
 
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad(){
 		MoodAnalyser moodAnalyzer = new MoodAnalyser("Iam in sad mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("SAD",mood);
+		String mood = null ;
+		try {
+			mood = moodAnalyzer.analyseMood();
+			Assert.assertEquals("SAD",mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Test
 	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("Iam in Happy mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@Test
 	public void givenMessage_WhenNull_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void givenMessage_WhenEmpty_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, e.type);
+			System.out.println(e.getMessage());
+		}
 	}
 }
