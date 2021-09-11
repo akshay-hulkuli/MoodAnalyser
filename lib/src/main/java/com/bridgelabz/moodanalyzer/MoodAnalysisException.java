cb9452bd13878bbe06c5e99bd6498972a683dd1f
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-11 20:11:46 +0530

    REFACTORED: class is made to extend Runtime Exception

--- lib/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalysisException.java
+++ lib/src/main/java/com/bridgelabz/moodanalyzer/MoodAnalysisException.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.moodanalyzer;
 
-public class MoodAnalysisException extends Exception{
+public class MoodAnalysisException extends RuntimeException{
 	enum ExceptionType{
 		ENTERED_NULL,ENTERED_EMPTY
 	}
